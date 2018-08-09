d0fe6c7c52f24606aa5b2196b3513cf311916997
Marco <marcopeix@outlook.com>
2018-08-08 22:50:44 -0400

    Event and key modifiers

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello World!'
+        title: 'Hello World!',
+        link: 'https://www.google.com',
+        finishedLink: '<a href="https://www.google.com">Google</a>',
+        counter: 0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        sayHello: function () {
+            this.title = 'Hello!';
+            return this.title;
+        },
+        increment: function (num, event) {
+            this.counter += num;
+        },
+        updateCoordinates: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
+        alertMe: function () {
+            alert('Yo');
+        }
     }
 });
+
+// LECTURE 15: TWO-WAY BINDING
