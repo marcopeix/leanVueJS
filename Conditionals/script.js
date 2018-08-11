a6bfefc404e491f44db21bb4890d75f2283eeb5a
Marco <marcopeix@outlook.com>
2018-08-11 10:21:08 -0400

    Completed conditionals and list section

---
+++ Conditionals/script.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'Marco', age: 23, color: 'red'},
+            {name: 'Lina', age: 23, color: 'Anything but red'}
+        ]
+    }
+})
