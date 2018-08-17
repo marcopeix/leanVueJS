50f08a66cecf443a8bafc31a0bce29ff4e35e746
Marco <marcopeix@outlook.com>
2018-08-16 22:19:18 -0400

    Initial commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Hello world!</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+}
+</script>
+
+<style>
+</style>
