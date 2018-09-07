3bfe6d9467d99e3833be30291f097d67813ee44b
Marco <marcopeix@outlook.com>
2018-09-06 21:49:47 -0400

    Custom directive

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Built-In Directives</h1>
+          <p v-text="'Some text'"></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Custom Directives</h1>
+          <p v-highlight>Color this</p>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- directives/src/main.js
+++ directives/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = 'green';
+  }
+});
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
