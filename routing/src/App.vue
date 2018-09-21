c5f4befd1c6bdd8f273a5aa642ea9294c526ad6d
Marco <marcopeix@outlook.com>
2018-09-20 22:39:15 -0400

    Started routing section

---
+++ routing/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+  <div class="ro">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <app-header></app-header>
+      <h1>Routing</h1>
+      <hr>
+      <router-view></router-view>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+import Header from './components/Header'
+
+export default {
+  components: {
+    appHeader: Header
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ routing/src/components/user/UserStart.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>
+            Select a user
+        </p>
+        <hr>
+        <ul class="list-group">
+            <li class="list-group-item" style="cursor: pointer">User 1</li>
+            <li class="list-group-item" style="cursor: pointer">User 2</li>
+            <li class="list-group-item" style="cursor: pointer">User 3</li>
+        </ul>
+    </div>
+</template>
