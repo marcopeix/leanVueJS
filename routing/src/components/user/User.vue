36fbae0a0f926cafb5d0e78205a73bd77d183d8f
Marco <marcopeix@outlook.com>
2018-09-22 10:39:24 -0400

    Continued vue-router section

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+        <h1>The User Page</h1>
+        <button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
+        <p>Loaded ID: {{ id }}</p>
+        <hr>
+        <router-view></router-view>
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            this.$router.push({name: 'home'});
+        }
+    }
+}
+</script>
+
